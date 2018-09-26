d131ad93c33a89a1c9558465d7061414d18d2128
James Moore <pbnjames@jamess-mbp.lan>
2018-09-26 18:51:28 -0400

    Added garage door and the open command

--- src/RemoteControlTest.java
+++ src/RemoteControlTest.java
@@ -2,9 +2,13 @@ public class RemoteControlTest { //Client
     public static void main(String[] args){
         SimpleRemoteControl remote = new SimpleRemoteControl(); //Invoker
         Light light = new Light(); //Receiver
+        GarageDoor garageDoor = new GarageDoor();//Receiver2
         LightOnCommand lightOn = new LightOnCommand(light); //Command
+        GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);//Command2
 
         remote.setCommand(lightOn); //Pass command to Invoker
+        remote.buttonWasPressed();
+        remote.setCommand(garageOpen);
         remote.buttonWasPressed();
     }
 }
