a10cb6626ebf15d041c56f5b9158fee85f560b99
James Moore <pbnjames@jamess-mbp.lan>
2018-10-01 18:31:18 -0400

    Implemented remote control, added stereo, updated test harness

---
+++ src/NoCommand.java
@@ -0,0 +1,9 @@
+public class NoCommand implements Command {
+
+    public void noCommand(){}
+
+    @Override
+    public void execute() {
+
+    }
+}
--- src/RemoteControlTest.java
+++ src/RemoteControlTest.java
@@ -1,14 +1,33 @@
+import java.rmi.Remote;
+
 public class RemoteControlTest { //Client
     public static void main(String[] args){
-        SimpleRemoteControl remote = new SimpleRemoteControl(); //Invoker
-        Light light = new Light(); //Receiver
-        GarageDoor garageDoor = new GarageDoor();//Receiver2
-        LightOnCommand lightOn = new LightOnCommand(light); //Command
-        GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);//Command2
+        RemoteControl remote = new RemoteControl(); //Invoker
 
-        remote.setCommand(lightOn); //Pass command to Invoker
-        remote.buttonWasPressed();
-        remote.setCommand(garageOpen);
-        remote.buttonWasPressed();
+        Light livingRoomLight = new Light();
+        GarageDoor garageDoor = new GarageDoor();
+        Stereo stereo = new Stereo("Living Room");
+
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+
+        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
+        GarageDoorCloseCommand garageDoorCloseCommand = new GarageDoorCloseCommand(garageDoor);
+
+        StereoOnWithCDCommand stereoOnWithCDCommand = new StereoOnWithCDCommand(stereo);
+        StereoOffCommand stereoOffCommand = new StereoOffCommand(stereo);
+
+        remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remote.setCommand(1, garageDoorOpenCommand, garageDoorCloseCommand);
+        remote.setCommand(2, stereoOnWithCDCommand, stereoOffCommand);
+
+        System.out.println(remote);
+
+        remote.onButtonWasPushed(0);
+        remote.offButtonWasPushed(0);
+        remote.onButtonWasPushed(1);
+        remote.offButtonWasPushed(1);
+        remote.onButtonWasPushed(2);
+        remote.offButtonWasPushed(2);
     }
 }
